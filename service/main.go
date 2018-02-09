ea583c77723fa63f49dd3560db6340c966e6ac5a
DexDexinWang <edgar_wdx@hotmail.com>
2018-02-09 13:05:45 -0800

    add big table inserstion

--- service/main.go
+++ service/main.go
@@ -9,17 +9,20 @@ import (
 	"reflect"
 	elastic "gopkg.in/olivere/elastic.v3"
 	"github.com/pborman/uuid"
+	"strings"
+	"context"
+	"cloud.google.com/go/bigtable"
 )
 
 const (
+	//update to ES
 	INDEX = "around"
 	TYPE = "post"
 	DISTANCE = "200km"
-	// Needs to update
-	//PROJECT_ID = "around-xxx"
-	//BT_INSTANCE = "around-post"
-	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.196.137.189:9200"
+	ES_URL = "http://35.231.109.82:9200"
+	//update to BT
+	PROJECT_ID = "prime-mechanic-194222"
+	BT_INSTANCE = "around-post"
 )
 
 
@@ -90,6 +93,33 @@ func handlerPost (w http.ResponseWriter, r *http.Request) {
 	id := uuid.New()
 	// Save to ES.
 	saveToES(&p, id)
+	saveToBT(&p, id)
+}
+
+func saveToBT(p *Post,id string){
+
+	ctx := context.Background()
+	// you must update project name here
+	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+	if err != nil {
+		panic(err)
+		return
+	}
+	tbl := bt_client.Open("post")
+	mut := bigtable.NewMutation()
+	t:= bigtable.Now()
+
+	mut.Set("post", "user", t, []byte(p.User))
+	mut.Set("post", "message", t, []byte(p.Message))
+	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+	err = tbl.Apply(ctx, id, mut)
+	if err != nil {
+		panic(err)
+		return
+	}
+	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
 
 }
 
@@ -166,8 +196,10 @@ func handlerSearch (w http.ResponseWriter, r *http.Request) {
 	for _, item := range searchResult.Each(reflect.TypeOf(typ)) { // instance of
 		p := item.(Post) // p = (Post) item
 		fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
-		// TODO(student homework): Perform filtering based on keywords such as web spam etc.
-		ps = append(ps, p)
+		//filter some posts which contains bad words
+		if !containsBadWords(&p.Message){
+			ps = append(ps, p)
+		}
 
 	}
 	js, err := json.Marshal(ps)
@@ -179,6 +211,14 @@ func handlerSearch (w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(js)
+}
 
-
+func containsBadWords(s *string ) bool {
+	filteredWords :=[] string{"fuck","bitch","nigger"}
+	for _, word := range filteredWords{
+		if strings.Contains(*s, word) {
+			return true;
+		}
+	}
+	return false;
 }
